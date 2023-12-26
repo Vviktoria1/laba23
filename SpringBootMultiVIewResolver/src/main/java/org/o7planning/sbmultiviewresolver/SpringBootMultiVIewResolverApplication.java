61c10dd21f6bcf86b1002858c4c666217ab28aca
Віка Зварко <114394185+Vviktoria1@users.noreply.github.com>
2023-12-26 22:30:52 +0200

    Add files via upload

---
+++ SpringBootMultiVIewResolver/src/main/java/org/o7planning/sbmultiviewresolver/SpringBootMultiVIewResolverApplication.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbmultiviewresolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiVIewResolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiVIewResolverApplication.class, args);
+	}
+
+}
